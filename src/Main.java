78b3e9fc58406a4d57a398e36e2cc8b57fb3a4d5
Vita Iskovskikh <vitaiskovskikh@gmail.com>
2022-08-06 19:24:26 +0300

    Create a project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 150;//стоимости билета
+        int bonus = 20;//количество рублей для одной бонусной мили
+
+
+        int miles = price/bonus;//количество бонусных миль
+        System.out.println(miles + "миль");
+    }
+}
